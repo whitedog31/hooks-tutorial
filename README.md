281775ea73a1b7b060ad38a61cba1f2281777f83
김민수 <jasonkim@mondrian.ai>
2022-03-04 17:45:54 +0900

    feat: using useContext

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Hooks tutorial
+
+1. useEffect 의 cleanup에 업데이트 되기 직전의 값을 보여주고 있다.
---
+++ src/ContextApp.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ContextSample from "./ContextSample";
+
+export default function ContextApp() {
+  return (
+    <div
+      style={{
+        border: "1px solid red",
+      }}
+    >
+      <ContextSample />
+    </div>
+  );
+}
